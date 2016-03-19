62b1a56413ab453ea7c85818ca3bece7ee006b67
dykyiivan <dykyiivan@gmail.com>
2016-03-19 21:05:59 +0200

    completion

--- our-web-site/js/common.js
+++ our-web-site/js/common.js
@@ -15,3 +15,16 @@ $(document).ready(function(){
 	});
 
 });
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: false, //Set AutoPlay to 3 seconds
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+})
