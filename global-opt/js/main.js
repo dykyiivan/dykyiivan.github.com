8584b8b1a4de28b12cd29c3c4cb6444e6df4114b
dykyiivan <dykyiivan@gmail.com>
2020-03-18 19:04:56 +0200

    upload new version of theme

--- global-opt/js/main.js
+++ global-opt/js/main.js
@@ -1,22 +1,22 @@
-$(function(){
-     // Scroll
-     var margin = 0;
-     $("a").click(function() { 
-      $("html, body").animate({
-         scrollTop: $($(this).attr("href")).offset().top+margin+ "px"
-      }, {
-         duration: 1600, 
-         easing: "swing"
-      });
-      return false;
-   });
+$(function () {
+ // Scroll
+  var margin = 0;
+  $("a").click(function () {
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top + margin + "px"
+    }, {
+      duration: 1600,
+      easing: "swing"
+    });
+    return false;
+  });
 
-// Slick Slider
+  //Slider
   $('.reviews-slider').slick({
-     slidesToShow: 3,
-     prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="img/next-arrow.png"></button>',
-     responsive: [
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/next-arrow.png"></button>',
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
@@ -27,38 +27,59 @@ $(function(){
           arrows: false
         }
       },
-      
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: false,
-        slidesToShow: 1,
-        dots: true
+
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 1,
+          dots: true
+
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: false,
-        slidesToShow: 1,
-        dots: true
-  
-      }
-    }
-  ]
+    ]
   });
-  // Mobile Menu
-  $('.header__burger').click(function() {
+
+  //Burger Menu
+  $('.header__burger').click(function () {
     $('.header__burger,.menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
-
-  // Wow.js
-  new WOW().init();
-
-
-
+  $(".menu__close").on("click", function () {
+    $('.header__burger,.menu').toggleClass('active');
+  });
 
 });
+
+//Wow Js
+new WOW().init();
+
+//Modal
+$("[data-modal=call]").on("click", function () {
+  $(".overlay, #call").fadeIn();
+});
+$(".modal__close").on("click", function () {
+  $(".overlay, #call").fadeOut("slow");
+});
+
+// To Top Button
+let btn = $('#to-top__link');
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+
